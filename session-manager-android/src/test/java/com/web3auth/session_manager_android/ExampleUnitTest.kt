87a150f040ab2ec47429bbf0a58a575518be8e5a
Gaurav Goel <gaurav@tor.us>
2023-09-14 09:02:10 +0530

    change algorithm transformation
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- session-manager-android/src/test/java/com/web3auth/session_manager_android/ExampleUnitTest.kt
+++ session-manager-android/src/test/java/com/web3auth/session_manager_android/ExampleUnitTest.kt
@@ -1,13 +1,8 @@
 package com.web3auth.session_manager_android
 
-import com.google.gson.Gson
 import com.web3auth.session_manager_android.keystore.KeyStoreManager
-import com.web3auth.session_manager_android.types.AES256CBC
-import com.web3auth.session_manager_android.types.ShareMetadata
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
-import java.nio.charset.StandardCharsets
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -25,17 +20,6 @@ class ExampleUnitTest {
         val newSessionKey = SessionManager.generateRandomSessionKey()
         val data = "a"
         val ephemKey = "04" + KeyStoreManager.getPubKey(newSessionKey)
-        val ivKey = KeyStoreManager.randomString(16)
-        val aes256cbc = AES256CBC(
-            newSessionKey,
-            ephemKey,
-            ivKey
-        )
-
-        val encryptedData = aes256cbc.encrypt(data.toByteArray(StandardCharsets.UTF_8))
-        val mac = aes256cbc.macKey
-        val encryptedMetadata = ShareMetadata(ivKey, ephemKey, encryptedData, mac)
-        val gson = Gson()
-        val gsonData = gson.toJson(encryptedMetadata)
+        val ivKey = KeyStoreManager.randomBytes(16)
     }
 }
